b27e66543bdb23aa99b12c8a594fc62c2524aab8
Uwiagod <sensigod466@gmail.com>
2026-01-12 20:36:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 console.log("Glory Cheat Hub Loaded - Welcome Developer");
 
 // We can add a mobile menu toggle here later if you want!
+// This makes the scrolling feel "weighted" and premium
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
